dd15541fcb3759212d4a90ff51f692cc7e811c60
DESKTOP-FSI8LT0\TUF <t.qucbo@gmail.com>
2023-11-03 17:03:08 +0700

    - Bấm chọn ghế thì hiện ra các ghế có trong từng rạp. - Chưa làm: 1. Không thể chọn ghế nếu ghế đó đã có đặt trước. 2. Bấm chọn 1 ghế chuyển sang trang Thanh toán vé.

--- Controllers/ChooseSeatController.cs
+++ Controllers/ChooseSeatController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using QuanLyRapPhim.Data;
 using QuanLyRapPhim.Models;
+using System.Linq;
 
 namespace QuanLyRapPhim.Controllers
 {
@@ -21,7 +22,26 @@ namespace QuanLyRapPhim.Controllers
         [Route("/chooseseat/{filmScheduleId}")]
         public IActionResult Index(string filmScheduleId)
         {
-            return View();
+            List<SeatModel> sms = (from sm in _context.seats
+                                    join crm in _context.cinemaRooms
+                                    on sm.cinemaRoomId equals crm.cinemaRoomId
+                                    join cm in _context.cinemas
+                                    on crm.cinemaId equals cm.cinemaId
+                                    join fsm in _context.filmSechedules
+                                    on crm.cinemaRoomId equals fsm.cinemaRoomId
+                                    where fsm.filmSecheduleId == filmScheduleId
+                                    orderby sm.seatRowChar, sm.seatColumnNumber
+                                    select sm).ToList();
+            return View(sms);
+        }
+
+        [HttpPost]
+        [Route("/getFilmScheduleDetail", Name = "getFilmScheduleDetail")]
+        public IActionResult GetFilmScheduleDetail(string filmScheduleId){
+            FilmSecheduleModel f = (from fsm in _context.filmSechedules
+                                    where fsm.filmSecheduleId == filmScheduleId
+                                    select fsm).FirstOrDefault();
+            return Json(f);
         }
     }
 }
